aa58a908340c8bda6914257ef41183eaf0ca64d6
Oliko136 <olchick136@gmail.com>
2025-10-06 16:15:48 +0400

    Update and set progress and timestamp

--- custom-video-player/script.js
+++ custom-video-player/script.js
@@ -18,16 +18,31 @@ function updatePlayIcon() {
         : play.innerHTML = `<i class="fa fa-pause fa-2x"></i>`;
 }
 
-// // Update progress & timestamp
-// function updateProgress() {
+// Update progress & timestamp
+function updateProgress() {
+    progress.value = (video.currentTime / video.duration) * 100;
 
-// }
+    // Get minutes
+    let mins = Math.floor(video.currentTime / 60);
 
-// // Set progress
+    if (mins < 10) {
+        mins = '0' + String(mins);
+    }
 
-// function setVideoProgress() {
+    // Get seconds
+    let secs = Math.floor(video.currentTime % 60);
 
-// }
+    if (secs < 10) {
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
+}
+
+// Set progress
+function setVideoProgress() {
+    video.currentTime = (+progress.value * video.duration) / 100;
+}
 
 // Stop video
 function stopVideo() {
@@ -39,8 +54,8 @@ function stopVideo() {
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
-//video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('timeupdate', updateProgress);
 
 play.addEventListener('click', toggleVideoStatus);
 stop.addEventListener('click', stopVideo);
-//progress.addEventListener('change', setVideoProgress);
+progress.addEventListener('change', setVideoProgress);
