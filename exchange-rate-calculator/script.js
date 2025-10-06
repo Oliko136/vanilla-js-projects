3e94fb8f7d4a226b0b7a9ae9bce811c7da5309c4
Oliko136 <olchick136@gmail.com>
2025-10-06 21:45:12 +0400

    Fetch rates, update UI and swap currencies

---
+++ exchange-rate-calculator/script.js
@@ -0,0 +1,42 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+const API_KEY = '141838ef736e4ebe22a5a223';
+
+// Fetch exchange rates and update the DOM
+function calculate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currency_one}`)
+        .then(res => res.json())
+        .then(data => {
+            const rate = data.conversion_rates[currency_two];
+
+            rateEl.innerHTML = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+            amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+        });
+}
+
+// Swap currencies
+function swapCurrencies() {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+
+    calculate();
+}
+
+// Event listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+swap.addEventListener('click', swapCurrencies);
+
+calculate();
