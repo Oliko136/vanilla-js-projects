9f76179e46233cfb5e279692360113d20ded9367
Oliko136 <olchick136@gmail.com>
2025-11-08 19:28:07 +0400

    Open and close modal

--- menu-slider-modal/script.js
+++ menu-slider-modal/script.js
@@ -8,5 +8,19 @@ function toggleNav() {
     document.body.classList.toggle('show-nav');
 }
 
+// Show modal
+function openModal() {
+    modal.classList.add('show-modal');
+}
+
+// Hide modal
+function closeModal() {
+    modal.classList.remove('show-modal');
+}
+
 // Event listeners
 toggle.addEventListener('click', toggleNav);
+open.addEventListener('click', openModal);
+close.addEventListener('click', closeModal);
+window.addEventListener('click',
+    e => e.target === modal ? closeModal() : false);
